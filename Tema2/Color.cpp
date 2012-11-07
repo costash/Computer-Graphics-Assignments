c744809d012143df74e8214ebbc7e1ad154fabe2
Constantin Șerban-Rădoi <costashsrc@gmail.com>
2012-11-07 18:36:07 +0200

    Add Color class.

---
+++ Tema2/Color.cpp
@@ -0,0 +1,18 @@
+#include "Color.h"
+
+// RGB color class
+
+Color::Color()
+	: r(0), g(0), b(0)
+{
+}
+
+Color::Color(float r, float g, float b)
+	: r(r), g(g), b(b)
+{
+}
+
+
+Color::~Color(void)
+{
+}
---
+++ Tema2/Color.h
@@ -0,0 +1,16 @@
+#pragma once
+
+// RGB color class
+
+class Color
+{
+public:
+	Color();
+	Color(float r, float g, float b);
+	~Color();
+public:
+	float r;	// red
+	float g;	// green
+	float b;	// blue
+};
+
